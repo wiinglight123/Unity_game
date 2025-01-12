ae23bccabd2d222d41d2c7d7332870d12030731e
akel <ene.cristian.p7q@student.ucv.ro>
2025-01-12 22:13:36 +0200

    Added death

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -8,7 +8,7 @@ public class GameOver : MonoBehaviour
     public bool Finish = false;
     private Animator animator;
     public string winningTag = "WinningSprite";
-
+    public bool Dies = false; 
     public string enemyTag = "Enemy";
 
     void Start()
@@ -37,13 +37,24 @@ public class GameOver : MonoBehaviour
     {
         player.position = respawnPosition;
         Debug.Log("Player has been respawned.");
+        Dies = false;
+        animator.SetBool("Dies", Dies);
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
-        {
-            RespawnPlayer();
+        {   
+            Dies = true;
+            animator.SetBool("Dies", Dies);
+            IEnumerator RespawnAfterDelay(float delay)
+            {
+                yield return new WaitForSeconds(delay);
+                RespawnPlayer();
+            }
+
+            // Inside OnCollisionEnter2D
+            StartCoroutine(RespawnAfterDelay(2.0f)); // Adjust the delay as needed
         }
     }
 
